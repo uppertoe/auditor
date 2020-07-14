9ac0efd08b695381bff6dcc11317c1dc05d78f9e
uppertoe <uppertoe@gmail.com>
2020-07-14 20:13:39 +1000

    authenticate with email address

--- auditor/accounts/admin.py
+++ auditor/accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Combines the user authentication and user profile in the admin
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'user profile'
+
+#Ensure that the email address is used as the username
+@admin.register(UserLogin)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    #Link the UserProfile to the User admin view
+    inlines = (UserProfileInline)
--- auditor/accounts/models.py
+++ auditor/accounts/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+#Set up the user and its attached profile
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('Please enter an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+#Override default User class to allow login with email address
+class UserLogin(AbstractUser):    
+    username = None
+    email = models.EmailField('email address', unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+#Profile information linked one-to-one with authentication details
+class UserProfile(models.Model):
+    user = models.OneToOneField(UserLogin, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.email
+
+#Listen for a User being saved to the database, and ensure it has an associated blank UserProfile
+@receiver(post_save, sender=UserLogin)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=UserLogin)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
